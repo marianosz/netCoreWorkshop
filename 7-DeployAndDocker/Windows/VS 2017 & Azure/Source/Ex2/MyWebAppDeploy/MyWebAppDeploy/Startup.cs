6745d1fa5e2e32587fc610f08bea35de643ac477
Mariano Sánchez <sanchezmariano@gmail.com>
2019-03-06 18:17:40 -0300

    Update Taks 8

--- 7-DeployAndDocker/Windows/VS 2017 & Azure/Source/Ex2/MyWebAppDeploy/MyWebAppDeploy/Startup.cs
+++ 7-DeployAndDocker/Windows/VS 2017 & Azure/Source/Ex2/MyWebAppDeploy/MyWebAppDeploy/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,7 +17,7 @@ namespace MyWebAppDeploy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
